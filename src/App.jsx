9ef1eafde2b7645893de0ae559998027b9276471
Otto Daniswara <ottodaniswara95@gmail.com>
2026-05-24 00:49:10 +0700

    update base url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 // ============================================================
 // CONFIG - ganti BASE_URL sesuai backend lo
 // ============================================================
-const BASE_URL = 'https://simada-kelompok-5.infinityfreeapp.com/api'; // sesuaikan dengan URL backend lo
+const BASE_URL = 'simada-proxy.ottodaniswara95.workers.dev/api'; // sesuaikan dengan URL backend lo
 
 const api = {
   post: async (endpoint, body, token) => {
